267de7d984f1a3739508482a057a710b2865698c
de-scientist <gitaumark502@gmail.com>
2025-10-24 20:32:41 +0300

    import { PrismaClient } from './generated/prisma';

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //import express from 'express'; 
 import express from 'express';
+//import { PrismaClient } from './generated/prisma';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
