d34df8f5f179589aabc508f3da435d7b4c67a893
de-scientist <gitaumark502@gmail.com>
2025-10-24 20:33:04 +0300

    Initialize Express app and Prisma Client

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import express from 'express';
 //import { PrismaClient } from './generated/prisma';
 import { PrismaClient } from '@prisma/client';
 
+// Initialize Express app and Prisma Client
 const app = express();
 const prisma = new PrismaClient();
